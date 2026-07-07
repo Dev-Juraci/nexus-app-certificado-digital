fababd0e16125196fbed78295c532a7e67dcb883
Dev-Juraci <devjuraci04@hotmail.com>
2026-07-07 01:32:27 -0300

    sharedPreferences Triggered

--- app/src/main/java/com/devsjura/file_apps/nexus_cdnuvem/ui/forget/RecoverActivity.kt
+++ app/src/main/java/com/devsjura/file_apps/nexus_cdnuvem/ui/forget/RecoverActivity.kt
@@ -1,17 +1,27 @@
 package com.devsjura.file_apps.nexus_cdnuvem.ui.forget
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.transition.Visibility
 import com.devsjura.file_apps.nexus_cdnuvem.R
 import com.devsjura.file_apps.nexus_cdnuvem.animations.AnimaStart
 import com.devsjura.file_apps.nexus_cdnuvem.databinding.ActivityRecoverBinding
 import com.devsjura.file_apps.nexus_cdnuvem.ui.login.LoginActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+import kotlin.time.Duration.Companion.milliseconds
 
 class RecoverActivity : AppCompatActivity() {
 
@@ -43,19 +53,44 @@ class RecoverActivity : AppCompatActivity() {
             }
 
             btnSendCode.setOnClickListener {
-                val input = inputEmail.text.toString()
+                val userEmail = inputEmail.text.toString()
 
-                //VERIFICAÇÃO DE FUNCIONAMENTO - sem progresso no codigo!!
+                val verifEmail = android.util.Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()
 
-                if (input.isNotBlank()) {
-                    Toast.makeText(this@RecoverActivity, "App informa: $input", Toast.LENGTH_SHORT)
-                        .show()
-                } else{
-                    Toast.makeText(this@RecoverActivity, "CAMPO VAZIO!! 😠👌👍", Toast.LENGTH_SHORT).show()
+
+                if (userEmail.isNotBlank() && verifEmail) {
+                    getSharedPreferences("meu_app_prefs", Context.MODE_PRIVATE).edit {
+                        putString("email_of_user", userEmail)
+                    }
+
+                    containerEmailRecovery.visibility = View.GONE
+                    containerVerificationCode.visibility = View.VISIBLE
+                    umTeste()
+
+                } else {
+                    Snackbar.make(
+                        binding.root,
+                        "Por favor, insira um e-mail válido",
+                        Snackbar.LENGTH_LONG
+                    ).show()
                 }
+
 
             }
         }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun umTeste() {
+        val sharedPreferencesTxt =
+            getSharedPreferences("meu_app_prefs", Context.MODE_PRIVATE).getString(
+                "email_of_user",
+                ""
+            )
+
+        binding.txtVerificationInstruction.text =
+            "Digite o código de 6 dígitos enviado para: $sharedPreferencesTxt"
 
     }
 }
