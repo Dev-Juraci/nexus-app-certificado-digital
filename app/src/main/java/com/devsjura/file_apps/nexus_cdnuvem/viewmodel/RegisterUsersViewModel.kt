afe6a5846f9be1c88a918f76f7be1231890a54d4
Dev-Juraci <devjuraci04@hotmail.com>
2026-08-11 00:05:36 -0300

    feat: add AuthViewModel with authentication states

--- app/src/main/java/com/devsjura/file_apps/nexus_cdnuvem/viewmodel/RegisterUsersViewModel.kt
+++ app/src/main/java/com/devsjura/file_apps/nexus_cdnuvem/viewmodel/RegisterUsersViewModel.kt
@@ -1,14 +1,75 @@
 package com.devsjura.file_apps.nexus_cdnuvem.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.devsjura.file_apps.nexus_cdnuvem.others.IdentificadorUtils
 import com.devsjura.file_apps.nexus_cdnuvem.repository.AuthRepository
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.launch
 
-class RegisterUsersViewModel(
-    private val authFirebase: AuthRepository = AuthRepository(),
-) : ViewModel() {
+sealed class AuthState {
+    object Idle : AuthState()
+    object LoadingAuth : AuthState()
+    object successAuth : AuthState()
+    data class Error(val msgErro: String) : AuthState()
+}
+
+class AuthViewModel(private val authViewModel: AuthRepository = AuthRepository()) : ViewModel() {
+
+    private val _stateRegistration = MutableLiveData<AuthState>(AuthState.Idle)
+    val stateRegistration: LiveData<AuthState> = _stateRegistration
+
+    private val _loginState = MutableLiveData<AuthState>(AuthState.Idle)
+    val loginState = MutableLiveData<AuthState>(AuthState.Idle)
+
+    fun registerUsers(
+        namesAuViMo: String,
+        emailAuViMo: String,
+        cpfAuViMo: String,
+        phoneAuViMo: String,
+        passwordAuViMo: String,
+    ) {
+
+        _stateRegistration.value = AuthState.LoadingAuth
+
+        viewModelScope.launch {
+
+            try {
+                authViewModel.signUpUsers(
+                    namesAuViMo,
+                    emailAuViMo,
+                    cpfAuViMo,
+                    phoneAuViMo,
+                    passwordAuViMo
+                )
+                _stateRegistration.value = AuthState.successAuth
+            } catch (e: Exception) {
+                _stateRegistration.value = AuthState.Error(e.message ?: "Erro ao cadastrar")
+            }
+
+
+        }
+
+
+    }
+
+    fun login(identificador: String, userSecure: String) {
+        _loginState.value = AuthState.LoadingAuth
+
+        viewModelScope.launch {
+            try {
+                val typeAuth = IdentificadorUtils.detectType(identificador)
+                authViewModel.login(identificador, userSecure, typeAuth)
+                _loginState.value = AuthState.successAuth
+
+            } catch (e: Exception) {
+                _loginState.value = AuthState.Error(e.message ?: "Credenciais inválidas")
+            }
+        }
+
+
+    }
 
 
 }
